ec2231735c308ebb94f3b08a5522ab6082011fbf
bshongwe <dru.da.beat.hustle@gmail.com>
2023-07-13 01:38:31 -0700

    Task 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+/**
+ * main - Entry point
+ *
+ * Return:zero (pass)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
