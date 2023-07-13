5b638b71987347518681d5fb45187121efd422b2
bshongwe <dru.da.beat.hustle@gmail.com>
2023-07-13 05:16:11 -0700

    Task 9 -Advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main -Entry point
- * Return: 1
+ * main - Entry point
+ * Return: Aways 0 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
