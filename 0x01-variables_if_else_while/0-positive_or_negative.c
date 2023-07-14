85f352ab08ebe6744578a4feefa550459a3084dd
bshongwe <dru.da.beat.hustle@gmail.com>
2023-07-14 03:40:27 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
- * main - check value of n status; print zero, positive or negative
- * Return Always 0 (Success)
+ * main - check value of n status: zero, positive, or negative
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -12,13 +12,20 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	/* your code goes here */
+
 	if (n > 0)
+	{
 		printf("%i is positive\n", n);
+	}
 	else if (n == 0)
+	{
 		printf("%i is zero\n", n);
+	}
 	else
-		printf("%i is negativee\n, n);
+	{
+		printf("%i is negative\n", n);
+	}
 
 	return (0);
 }
