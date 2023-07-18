cce6b8e375b6f4f5e7d032b14268409bb4e1d0f9
bshongwe <dru.da.beat.hustle@gmail.com>
2023-07-18 09:26:21 -0700

    Task 13 - Advanced

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,51 @@
 #include "main.h"
 
 /**
- * print_digit - Print a two-digit number or a single digit with leading zeros
- *
- * @num: Integer, the number to be printed
+ * print_times_table - prints times table
+ * @n : times table to use
+ * Return:void
  */
-void print_digit(int num)
-{
-	if (num < 10)
-	{
-		_putchar(' ');
-		_putchar('0' + num);
-	}
-	else
-	{
-		_putchar('0' + (num / 10));
-		_putchar('0' + (num % 10));
-	}
-}
 
-/**
- * print_times_table - Print the times table up to a given number
- *
- * @n: Integer, the upper bound for the times table (0 to 15)
- */
+
+
 void print_times_table(int n)
 {
-	if (n < 0 || n > 15)
-		return;
+int a = 0, rep, b;
 
-	int i, j, result;
-
-	for (i = 0; i <= n; i++)
-	{
-		_putchar('0');
-
-		for (j = 1; j <= n; j++)
-		{
-			result = i * j;
-			_putchar(',');
-			_putchar(' ');
-			print_digit(result);
-		}
-
-		_putchar('\n');
-	}
+if (n < 0 || n > 15)
+return;
+while (a <= n)
+{
+for (b = 0; b <= n; b++)
+{
+rep = a * b;
+if (b == 0)
+_putchar('0' + rep);
+else if (rep < 10)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + rep);
+}
+else if (rep < 100)
+{
+_putchar(' ');
+_putchar('0' + rep / 10);
+_putchar('0' + rep % 10);
+}
+else
+{
+_putchar('0' + rep / 100);
+_putchar('0' + (rep - 100) / 10);
+_putchar('0' + rep % 10);
+}
+if (b < n)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+a++;
+}
 }
