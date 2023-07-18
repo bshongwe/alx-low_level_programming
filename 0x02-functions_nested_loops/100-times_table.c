8864dd57ff2791c1058822c1e949111cca2783f7
bshongwe <dru.da.beat.hustle@gmail.com>
2023-07-18 08:14:46 -0700

    Task 13 - Advanceed Task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,12 +3,11 @@
 /**
  * print_times_table - print n times table
  * @n: integer
- * Return: void
  */
 void print_times_table(int n)
 {
 	int i, j, k;
-
+	
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
@@ -18,8 +17,9 @@ void print_times_table(int n)
 				k = j * i;
 				if (j == 0)
 				{
-					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
+					_putchar('0');
+				}
+				else if (k < 10)
 				{
 					_putchar(',');
 					_putchar(' ');
