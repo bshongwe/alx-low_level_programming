9f6995eaa4754308ab92a3f8338d7fb259b6f8a8
bshongwe <dru.da.beat.hustle@gmail.com>
2023-07-17 21:54:38 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - print _putchar
+ * Return: zero
+ */
+int main(void)
+{
+    char str[] = "_putchar";
+    int ch;
+
+    for (ch = 0; ch < 8; ++ch)
+        _putchar(str[ch]);
+
+    _putchar('\n');
+
+    return (0);
+}
