01637da3667121b6bdaaef21150ce4c61ac019ad
bshongwe <dru.da.beat.hustle@gmail.com>
2023-07-24 09:22:25 -0700

    Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,14 +3,15 @@
 
 /**
  * print_array - function prints elements of an array
- * @a: array name
+ * @a: array
  * @n: number of elements in array printed
- * Return: a and n inputs
+ * Return: Always 0 (Success)
  */
 void print_array(int *a, int n)
 {
 	int i;
 
+	i = 0;
 	for (i = 0; i < (n - 1); i++)
 	{
 		printf("%d, ", a[i]);
