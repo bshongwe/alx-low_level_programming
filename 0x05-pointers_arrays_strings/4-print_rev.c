31144e624cacb752552b693a0a04505b272af380
bshongwe <dru.da.beat.hustle@gmail.com>
2023-07-24 02:47:04 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse order
+ * @s: character pointer
+ * Return: Always 0 (Success)
+ */
+void print_rev(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+
+	while (a >= 0)
+	{
+		_putchar(s[a]);
+		a--;
+	}
+	_putchar('\n');
+}
