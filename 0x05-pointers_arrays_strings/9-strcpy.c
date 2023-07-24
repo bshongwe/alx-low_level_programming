dcdbc74c063124e3f9d65eff47a7787951ea4a76
bshongwe <dru.da.beat.hustle@gmail.com>
2023-07-24 11:15:35 -0700

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,13 +14,16 @@ char *_strcpy(char *dest, char *src)
 	int i, n;
 
 	n = strlen(src);
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (n > strlen(dest))
+	{
+		dest = malloc(n + 1);
+	}
+
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
-	for (; i < n; i++)
-	{
-		dest[i] = '\0';
-	}
+	dest[i] = '\0';
+
 	return (dest);
 }
