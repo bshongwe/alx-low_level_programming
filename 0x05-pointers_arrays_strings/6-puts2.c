cd459f8d30a21cec99bc3ec988598d8a14ad4531
bshongwe <dru.da.beat.hustle@gmail.com>
2023-07-24 07:59:17 -0700

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,16 +7,23 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
 
-	while (str[i] != '\0')
+	while (*y != '\0')
 	{
-		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
-
-		i++;
+		y++;
+		longi++;
 	}
-	_putchar('\n');
+	t = longi - 1;
+	for (o = 0; o <= t; o++)
+	{
+		if (o % 2)
+		{
+			_putchar(str[o]);
+		}
+	}
+		_putchar('\n');
 }
