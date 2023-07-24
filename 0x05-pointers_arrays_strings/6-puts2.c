70960e977fa09764c2b987f9dc2afcc59a943323
bshongwe <dru.da.beat.hustle@gmail.com>
2023-07-24 08:04:55 -0700

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * puts2 - prints a string
+ * puts2 - print only one character out of two
+ * starting with first one
  * @str: pointer to the string to print
  * Return: void
  */
@@ -20,7 +21,7 @@ void puts2(char *str)
 	t = longi - 1;
 	for (o = 0; o <= t; o++)
 	{
-		if (o % 2)
+		if (o % 2 == 0)
 		{
 			_putchar(str[o]);
 		}
