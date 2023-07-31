9ae250d678b43d112ab21f6c3688b02ce3e177e8
bshongwe <dru.da.beat.hustle@gmail.com>
2023-07-31 04:58:01 -0700

    Task 3 - while loop + for loop + if

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,31 +9,22 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j;
+	unsigned int j = 0;
+	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		for (i = 0; accept[i]; i++)
 		{
-			if (s[i] == accept[j])
+			if (*s == accept[1])
 			{
+				j++;
 				break;
 			}
+			else if (accept[i + 1] == '\0')
+				return (j);
 		}
-		if (accept[j] == '\0')
-		{
-			break;
-		}
+		s++;
 	}
-	return (i);
-}
-
-int main(void)
-{
-	char *s = "helloworld";
-	char *accept = "world";
-	int len = strspn(s, accept);
-
-	printf("The length of the prefix substring is %d\n", len);
-	return (0);
+	return (j);
 }
