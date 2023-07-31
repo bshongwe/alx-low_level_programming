4defa1935c231b47c07d0805ba9553f41aefd5dd
bshongwe <dru.da.beat.hustle@gmail.com>
2023-07-31 05:51:43 -0700

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function locates first occurrence in string s
+ * @s: first input
+ * @accept: second input
+ * Return: number of accepted bytes (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i;
+
+	for (i = 0; *s && accept[i]; i++)
+	{
+		if (*s == accept[i])
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	return ('\0');
+}
