7702c5001387653b17ccbccb918f667f86dd1943
bshongwe <dru.da.beat.hustle@gmail.com>
2023-07-30 23:25:13 -0700

    Task 0 - main.h, _putchar.c and 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memset - fill block of memory with constant value
+ * @s: memory address to be filled
+ * @b: constant value
+ * @n: number of bytes filled
+ * Return: pointer to filled memory s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	while (i < n)
+		*(s + i) = b;
+	i++;
+}
