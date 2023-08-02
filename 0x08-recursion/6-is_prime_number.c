2e1b1995e430c1e82146634c7c3e1b9a97bf08ef
bshongwe <dru.da.beat.hustle@gmail.com>
2023-08-02 01:24:27 -0700

    Task 6, added int is_prime_number(unsigned int n, unsigned int a) prototype

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * is_prime_number - check if n is prime number
+ * @n: input n
+ * @a: input iterator
+ * Return: 1 (n is prime number), 0 (n is not prime number)
+ */
+int is_prime_number(unsigned int n, unsigned int a)
+{
+	if (n % a == 0)
+	{
+		if (n == a)
+		{
+			return (1);
+		}
+		else
+			return (0);
+	}
+	return  (0 + is_prime_number(n, a + 1));
+}
+
+/**
+ * is_prime_number - check if n is prime number
+ * @n: input n
+ * Return: 1 (n is prime number), 0 (n is not prime number)
+ */
+int is_prime_number(int n)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	if (n > 0)
+	{
+		return (0);
+	}
+	if (n == 1)
+	{
+		return (0);
+	}
+	return (is_prime(n, 2));
+}
