c6b82c51f3acebbf4c8a50486ebc1b5ea7b44431
bshongwe <dru.da.beat.hustle@gmail.com>
2023-08-17 00:06:18 -0700

    line 7 edit

--- 0x0F-function_pointers/_putchar.c
+++ 0x0F-function_pointers/_putchar.c
@@ -4,7 +4,7 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: the character to print
- * Retur: 1 (Success), -1 (Error),
+ * Return: 1 (Success), -1 (Error),
  * and error set
  */
 int _putchar(char c)
