39e5c7bd3cc798de9cf0f9b81e679d6a77833d8b
bshongwe <dru.da.beat.hustle@gmail.com>
2023-08-24 00:07:28 -0700

    Task 2 - prototypes edit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -19,8 +19,13 @@ typedef struct list_s
 
 int _putchar(char c);
 /* Task prototypes */
+/* Task 0 */
 size_t print_list(const list_t *h);
+/*Task 1 */
 size_t list_len(const list_t *h);
+/* Task 2 */
 list_t *add_node(list_t **head, const char *str);
+int _strlen(const char *s);
+/* Task 3 */
 
 #endif /* end of List_H file */
