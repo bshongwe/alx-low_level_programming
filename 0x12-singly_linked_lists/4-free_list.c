5d8a6e221167bc7eea117e5ada9dfa50f2810558
bshongwe <dru.da.beat.hustle@gmail.com>
2023-08-24 00:50:11 -0700

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "list.h"
+
+/**
+ * free_list - func frees list_t list address
+ * @head: first list_t elems
+ * Return: void (Success), fails (NULL)
+ */
+void free_list(list_t *head)
+{
+	list_ *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -30,5 +30,7 @@ list_t *add_node(list_t **head, const char *str);
 int _strlen(const char *s);
 /* Task 3 */
 list_t *add_node_end(list_t **head, const char *str);
+/* Task 4 */
+void free_list(list_t *head);
 
 #endif /* end of List_H file */
