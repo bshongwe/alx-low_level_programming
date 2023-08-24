fd7a6cdfcc7b0151e3b9136ec8d988c40fa191de
bshongwe <dru.da.beat.hustle@gmail.com>
2023-08-24 01:43:04 -0700

    Task 6 - advanced

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,21 @@
+; Declare needed C functions
+
+	SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
+
+	SECTION .text
+	extern printf
+	global main
+main
+	push	rbp
+
+	mov	rdi,fmt
+	mov	rsi,msg
+	mov	rax,0
+	call	printf
+
+	pop	rbp
+
+	mov	rax,0
+	ret
