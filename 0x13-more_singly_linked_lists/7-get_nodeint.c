ce089f18c0319ca161d6cfe52c0580d31f87beed
bshongwe <dru.da.beat.hustle@gmail.com>
2023-08-28 01:08:16 -0700

    Task 7 - with prototype

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - func prints nth node pointer on linked list
+ * @head: list head
+ * @index: node index
+ * Return: nth node pointer (Success), node does not exist (NULL)
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+{
+	unsigned int i = 0;
+
+	for (i = 0; i < index && head != NULL; i++)
+		head =  head->next;
+
+	return (head);
+}
