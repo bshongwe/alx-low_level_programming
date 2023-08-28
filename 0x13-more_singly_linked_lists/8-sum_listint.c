5c467c3970575eb6b43835f2462b850f2cd3d151
bshongwe <dru.da.beat.hustle@gmail.com>
2023-08-28 01:40:28 -0700

    Task 8 - with prototype

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint - func prints all sum n data in linked list
+ * @head: list head
+ * Return: all n data sum (Success), fail (NULL),
+ * 0 (list is empty)
+ */
+int sum_listint(listint_t *head);
+{
+	int sum_data;
+
+	sum_data = 0;
+
+	while (head != NULL)
+	{
+		sum_data += head->n;
+		head = head->next;
+	}
+
+	return (sum_data);
+}
