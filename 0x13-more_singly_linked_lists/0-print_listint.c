79bbefa081d302df06ece91d5dbf2de85ec5fc1e
bshongwe <dru.da.beat.hustle@gmail.com>
2023-08-27 22:01:58 -0700

    Task 0 - with prototypes, libraries

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - func print all list elems
+ * @h: first elmem on list
+ * Return: nodes num (Success), fail (NULL)
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes_num = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodes_num++;
+	}
+	return (nodes_num)
+}
