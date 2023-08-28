45b4c425f4fcc7b134713a5bcf74ead7c73e29c4
bshongwe <dru.da.beat.hustle@gmail.com>
2023-08-28 05:59:28 -0700

    Task 14 - Advanced - with prototypes

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - func locates loop in linked list
+ * @head: list head
+ * Return: loop init address node
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *prev;
+	listint_t *p2;
+
+	pre = head;
+	p2 = head;
+	while (head && p2 && p2->next)
+	{
+		head = head->next;
+		p2 = p2->next->next;
+
+		if (head == p2)
+		{
+			head = prev;
+			prev = p2;
+			while (1)
+			{
+				p2 = prev;
+				while (p2->next != head && p2->next != prev)
+					p2 = p2->next;
+				if (p2->next == head)
+				{
+					break;
+				}
+
+				head = head->next;
+			}
+			return (p2->next);
+		}
+	}
+
+	return (NULL);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -37,5 +37,6 @@ size_t print_listint_safe(const listint_t *head);
 void free_listp(listp_t **head);
 size_t free_listint_safe(listint_t **h);
 void free_listp2(listp_t **head);
+listint_t *find_listint_loop(listint_t *head);
 
 #endif /* end of lists.h file */
