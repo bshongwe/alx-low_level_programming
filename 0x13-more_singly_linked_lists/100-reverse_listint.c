316176c519344e8d7e1374282ecbb2b2d2598112
bshongwe <dru.da.beat.hustle@gmail.com>
2023-08-28 03:52:18 -0700

    Task 11 - Advanced - with prototype

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - func prints reversed linked list
+ * @head: list head
+ * Return: first node address pointer (SUccess), fail (NULL)
+ */
+listint_t *reverse_listint(listint_t **head);
+{
+	listint_t *n;
+	listint_t *p;
+
+	n = NULL;
+	p = NULL;
+
+	while (*head != NULL)
+	{
+		n = (*head)->next;
+		(*head)->next = p;
+		p = *head;
+		*head = n;
+	}
+
+	*head = p;
+	return (*head);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -19,7 +19,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* Task prototypes */
+/* Mandatory tasks prototypes */
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -31,5 +31,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+/* Advaned tasks prototypes */
+listint_t *reverse_listint(listint_t **head);
 
 #endif /* end of lists.h file */
