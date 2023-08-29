f44da07f8278133a30d2469489fbad9199b56963
bshongwe <dru.da.beat.hustle@gmail.com>
2023-08-28 22:41:31 -0700

    _putchar.c and library edit

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,6 +5,18 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ * Return: 1 (Success), -1 (Error),
+ * and error set
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
  * struct listp_s - singly linked list
