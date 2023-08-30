002914ada8b59824e92a69e1b2fc37d08deeafe3
bshongwe <dru.da.beat.hustle@gmail.com>
2023-08-29 22:20:25 -0700

    _putchar.c def removal

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -8,17 +8,6 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: the character to print
- * Return: 1 (Success), -1 (Error),
- * and error set
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
  * struct listp_s - singly linked list
  * @p: node pointers
  * @next: next node point
