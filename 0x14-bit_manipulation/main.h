869a7b07a90377da584f0ba32b817d2ae6b8c408
bshongwe <dru.da.beat.hustle@gmail.com>
2023-08-31 03:08:10 -0700

    Library

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,9 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+/* Library addition */
+#include <stdio.h>
+
 /* Mandatory tasks prototypes */
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
