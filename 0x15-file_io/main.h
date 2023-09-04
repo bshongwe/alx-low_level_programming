ebe4774675bf612aa706859496b727607cbfbb51
bshongwe <dru.da.beat.hustle@gmail.com>
2023-09-04 01:46:03 -0700

    Task 4 - Advanced

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,6 +10,7 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <elf.h>
 
 /* Mandatory prototypes */
 int _puthar(char c);
@@ -19,9 +20,21 @@ int append_text_to_file(const char *filename, char *text_content);
 
 /* Advanced tasks */
 int main(int argc, char *argv[]);
+int main(int argc, const char *argv[]);
 
 /* Helper */
 int _strlen(char *s);
 void error_handler(int exit_code, char *message, char type, ...);
+void elf_entry(const unsigned char *buff, size_t bit_mode, int big_endian);
+void elf_type(const unsigned char *buff, int big_endian);
+void elf_abivers(const unsigned char *buff);
+void elf_osabi(const unsigned char *buff);
+void elf_version(const unsigned char *buff);
+int elf_data(const unsigned char *buff);
+size_t elf_class(const unsigned char *buff);
+void elf_magic(const unsigned char *buff);
+void _read(int fd, char *buf, size_t count);
+void _close(int fd);
+int _strncmp(const char *s1, const char *s2, size_t n);
 
 #endif /* end of MAIN_H file */
