61148750b1234ed10f968a4320c9278e3800b474
bshongwe <dru.da.beat.hustle@gmail.com>
2023-09-04 06:22:13 -0700

    Task 0 - alternative tryout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,32 +9,27 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
+	ssize_t nrd, nwr;
 	char *buff;
-	ssize_t bytes_rd, bytes_wr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 		return (0);
 
-	buff = malloc(sizeof(char) * letters + 1);
-	if (buff == NULL)
+	buff = malloc(sizeof(char) * (letters));
+	if (!buff)
 		return (0);
 
-	bytes_rd = read(fd, buff, letters);
-	if (bytes_rd == -1)
-		return (0);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-	buff[letters + 1] = '\0';
 	close(fd);
-
-	bytes_wr = write(STDOUT_FILENO, buff, bytes_rd);
-	if (bytes_wr == -1)
-		return (0);
 
 	free(buff);
 
-	return (bytes_rd);
+	return (nwr);
 }
