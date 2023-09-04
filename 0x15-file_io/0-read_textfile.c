4631dddbb00c156f0adf593bf130e79fdbd01b33
bshongwe <dru.da.beat.hustle@gmail.com>
2023-09-04 07:53:47 -0700

    Task 0 - Revert code

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,16 +1,16 @@
 #include "main.h"
-#include <math.h>
 
 /**
  * read_textfile - func reads text file and prints chars to stdout
  * @filename: filename to read
  * @letters: chars to read
- * Return: number of chars read or 0 if fail
+ * Return: number of chars read (Success), error (0)
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int f, bytes_rd, i, bytes_wr;
+	int fd;
 	char *buff;
+	ssize_t bytes_rd, bytes_wr;
 
 	if (filename == NULL)
 		return (0);
@@ -19,22 +19,21 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
-	buff = malloc(sizeof(char) * letters);
+	buff = malloc(sizeof(char) * letters + 1);
 	if (buff == NULL)
 		return (0);
 
 	bytes_rd = read(fd, buff, letters);
-	buff[letters + 1] = '\0';
-
-	for (i = 0; buff[i] != '\0'; i += 1)
-		bytes_rd += 1;
-
-	bytes_wr = close(f);
-	if (res != 0)
-		exit(-1);
-	bytes_wr = write(STDOUT_FILENO, buff, bytes_rd);
-	if (bytes_wr != bytes_rd)
+	if (bytes_rd == -1)
 		return (0);
+
+	buff[letters + 1] = '\0';
+	close(fd);
+
+	bytes_wr = write(STDOUT_FILENO, buff, bytes_rd);
+	if (bytes_wr == -1)
+		return (0);
+
 	free(buff);
 
 	return (bytes_rd);
