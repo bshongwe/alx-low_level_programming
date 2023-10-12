5b38327f5cf892e730b53ad1573b81dd81fa0759
bshongwe <dru.da.beat.hustle@gmail.com>
2023-10-12 03:46:10 -0700

    Task 8 - correction

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,41 +9,41 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *hd_1;
-	dlistint_t *hd_2;
+	dlistint_t *h1;
+	dlistint_t *h2;
 	unsigned int a;
 
-	hd_1 = *head;
+	h1 = *head;
 
-	if (hd_1 != NULL)
-		while (hd_1->prev != NULL)
-			hd_1 = h1->prev;
+	if (h1 != NULL)
+		while (h1->prev != NULL)
+			h1 = h1->prev;
 
 	a = 0;
 
-	while (hd_1 != NULL)
+	while (h1 != NULL)
 	{
 		if (a == index)
 		{
 			if (a == 0)
 			{
-				*head = hd_1->next;
+				*head = h1->next;
 				if (*head != NULL)
 					(*head)->prev = NULL;
 			}
 			else
 			{
-				hd_2->next = hd_1->next;
+				h2->next = h1->next;
 
-				if (hd_1->next != NULL)
-					hd_1->next->prev = hd_2;
+				if (h1->next != NULL)
+					h1->next->prev = h2;
 			}
 
-			free(hd_1);
+			free(h1);
 			return (1);
 		}
-		hd_2 = hd_1;
-		hd_1 = hd_1->next;
+		h2 = h1;
+		h1 = h1->next;
 		a++;
 	}
 
