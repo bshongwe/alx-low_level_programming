0ddc3c9e1b9cb4e15e90d0787ff806f3ebc50fc3
bshongwe <dru.da.beat.hustle@gmail.com>
2023-10-16 01:55:36 -0700

    Task 3 - Advanced - I messed up

--- 0x18-dynamic_libraries/extra-project-files/101-make_me_win.sh
+++ 0x18-dynamic_libraries/extra-project-files/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. wget -P .. https://github.com/bshongwe/alx-low_level_programming/blob/0adbd5742e552ccb583c1ceccc0276d89f7a2a36/0x18-dynamic_libraries/libgiga.so 
-export LD_PRELOAD="$PWD/../libgiga.so"
+wget -P .. wget -P .. https://github.com/bshongwe/alx-low_level_programming/blob/9a12f7cae8ee50ba0bdc81ade444b760bf7006a6/0x18-dynamic_libraries/lib-jackpot.so
+export LD_PRELOAD="$PWD/../lib-jackpot.so"
