b9393cce1f1e27f551b596e539825a821175755a
bshongwe <dru.da.beat.hustle@gmail.com>
2024-02-04 22:28:08 -0800

    Task 7 - Advanced: search for value in sorted array using the Jump search algorithm

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,6 +3,7 @@
 
 /* Libraries */
 #include <stdio.h>
+#include <math.h>
 
 /* Structs */
 
@@ -23,10 +24,10 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-
 /* Prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 /* Helpers */
 int recursive_search(int *array, size_t size, int value);
