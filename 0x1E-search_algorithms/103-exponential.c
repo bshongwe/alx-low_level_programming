af718533f7d94d649e59397d2852cc17e693e8b3
bshongwe <dru.da.beat.hustle@gmail.com>
2024-02-07 08:58:58 -0800

    Task 10 - Advanced - Exponential search reco

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,12 +1,12 @@
 #include "search_algos.h"
 
 /**
-  * _binary_search - Func searches for sorted array int value
-  *                  using binary search.
-  * @array: Array first element pointer to search.
-  * @left: Starting index of the [sub]array to search.
-  * @right: Ending index of the [sub]array to search.
-  * @value: Search value.
+  * _binary_search - Searches for a value in a sorted array
+  *                  of integers using binary search.
+  * @array: A pointer to the first element of the array to search.
+  * @left: The starting index of the [sub]array to search.
+  * @right: The ending index of the [sub]array to search.
+  * @value: The value to search for.
   *
   * Return: If the value is not present or the array is NULL, -1.
   *         else, the index where the value is located.
@@ -18,9 +18,7 @@ int _binary_search(int *array, size_t left, size_t right, int value)
 	size_t i;
 
 	if (array == NULL)
-	{
 		return (-1);
-	}
 
 	while (right >= left)
 	{
@@ -42,11 +40,11 @@ int _binary_search(int *array, size_t left, size_t right, int value)
 }
 
 /**
-  * exponential_search - Func searches for value in sorted array
+  * exponential_search - Searches for a value in a sorted array
   *                      of integers using exponential search.
-  * @array: Array first element pointer to search.
-  * @size: Number of elements in array.
-  * @value: Search value.
+  * @array: A pointer to the first element of the array to search.
+  * @size: The number of elements in the array.
+  * @value: The value to search for.
   *
   * Return: If the value is not present or the array is NULL, -1.
   *         Otherwise, the index where the value is located.
@@ -58,9 +56,7 @@ int exponential_search(int *array, size_t size, int value)
 	size_t i = 0, right;
 
 	if (array == NULL)
-	{
 		return (-1);
-	}
 
 	if (array[0] != value)
 	{
